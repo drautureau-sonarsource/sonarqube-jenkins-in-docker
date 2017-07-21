2c23e444bb836822e46dc3565740314cabf3d168
David Rautureau <david.rautureau@sonarsource.com>
2017-07-21 11:52:23 +0200

    Ensure that SonarQube is healthy before creating the webhook

--- sonarqube/check-running.sh
+++ sonarqube/check-running.sh
@@ -1,4 +1,5 @@
-ret=$(curl -f http://localhost:9000/api/system/ping)
-if [ "$ret" != "pong" ]; then
+response=$(curl -s -f http://localhost:9000/api/system/status)
+status=$(echo $response | jq -r '.status')
+if [ "$status" != "UP" ]; then
   exit 1
 fi
